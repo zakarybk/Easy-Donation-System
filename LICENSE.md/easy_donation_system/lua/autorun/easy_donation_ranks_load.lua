060639057748155fc55ed52aa9ba8706892c98df
Hackcraft <zakary@zakary.org>
2017-06-25 11:13:06 +0100

    Delete easy_donation_ranks_load.lua

--- LICENSE.md/easy_donation_system/lua/autorun/easy_donation_ranks_load.lua
+++
@@ -1,9 +0,0 @@
-if SERVER then
-	AddCSLuaFile("easy_donator_ranks/config.lua")
-	AddCSLuaFile("easy_donator_ranks/cl_eds.lua")
-	include("easy_donator_ranks/config.lua")
-	include("easy_donator_ranks/sv_eds.lua")
-else
-	include("easy_donator_ranks/config.lua")
-	include("easy_donator_ranks/cl_eds.lua")
-end
